4214d1e6f67eef2a6132ee31320fe5405ed94edb
Wojciech Cywinski <wcywin@gmail.com>
2020-10-13 23:28:08 +0200

    add CRUD create, update and delete mutations

--- src/resolvers/User.js
+++ src/resolvers/User.js
@@ -2,7 +2,7 @@ const User = {
     posts(parent, args, { db }, info) {
     return db.posts.filter(post => post.author === parent.id)
   },
-  comments(parent, args, ctx, info) {
+  comments(parent, args, { db }, info) {
     return db.comments.filter(comment => comment.author === parent.id)
   }
 }
